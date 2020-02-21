f76e895393427b108f1cfdaada1b4f487e1437d4
jonathan <jonathan.j.rayner@gmail.com>
2020-02-21 21:44:25 +0200

    Grouped neural network code and functions to class balance and create frequency features as separate .py files

---
+++ nn.py
@@ -0,0 +1,169 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow import keras
+
+# import preprocessing and plotting helper functions
+import nn_functions
+
+# Import and split the data
+train = pd.read_csv('train.csv')
+
+# The ID_code column contains no information, so we remove it
+train.drop('ID_code', axis=1, inplace=True)
+
+train, validation = train_test_split(train, test_size=0.2)
+
+train_labels = train.pop('target')
+validation_labels = validation.pop('target')
+
+# Preprocessing
+# make a copy of trainand and validation, scaled to [0,1]
+train_scaled = train.copy()
+scaler = MinMaxScaler()
+train_scaled = scaler.fit_transform(train_scaled)
+train_scaled = scaler.fit_transform(train_scaled)
+train_scaled = pd.DataFrame(train_scaled).reset_index(drop=True)
+
+validation_scaled = validation.copy()
+validation_scaled = scaler.transform(validation_scaled)
+validation_scaled = pd.DataFrame(validation_scaled).reset_index(drop=True)
+
+# Create frequency features
+frequency_features = FrequencyFeatures()
+
+# number of decimal places we'll round everything to
+decimals = 4
+
+frequency_features.fit(train_dataframe=train, decimals=decimals)
+
+train_new_features = frequency_features.transform(dataframe=train).reset_index(drop=True)
+validation_new_features = frequency_features.transform(dataframe=validation).reset_index(drop=True)
+
+# combine scaled features and new features (we reset the index of everything earlier)
+train = pd.concat([train_scaled, train_new_features], axis=1)
+validation = pd.concat([validation_scaled, validation_new_features], axis=1)
+
+# uncomment to use class balancing
+# majority_class_ratio = 7.0
+# oversample_rate = 0.25
+
+# train, train_labels = change_class_balance(train_dataframe=train,
+#                                                    train_labels=train_labels,
+#                                                    oversample_rate=oversample_rate,
+#                                                    majority_class_ratio=majority_class_ratio)
+
+# tensorflow needs numpy arrays
+train = np.array(train_copy)
+validation = np.array(validation_copy)
+
+train_labels = np.array(train_labels)
+validation_labels = np.array(validation_labels)
+
+# reshape so that new features are paired with corresponding original features
+train = pair_features(train)
+validation = pair_features(validation)
+
+# clear keras session so that we can rerun without errors
+tf.keras.backend.clear_session()
+
+METRICS = [
+    keras.metrics.TruePositives(name='tp'),
+    keras.metrics.FalsePositives(name='fp'),
+    keras.metrics.TrueNegatives(name='tn'),
+    keras.metrics.FalseNegatives(name='fn'),
+    keras.metrics.BinaryAccuracy(name='accuracy'),
+    keras.metrics.Precision(name='precision'),
+    keras.metrics.Recall(name='recall'),
+    keras.metrics.AUC(name='auc'),
+]
+
+N_TRAIN = train_copy.shape[0]
+BATCH_SIZE = 64
+EPOCHS = 100
+STEPS_PER_EPOCH = N_TRAIN//BATCH_SIZE
+
+# take some ideas from https://www.tensorflow.org/tutorials/keras/overfit_and_underfit
+
+# learning rate decay
+# initial learning rate
+initial_rate = 0.01
+
+# 'decay_factor' = x means learning rate decays to 1/2 of the 'initial_rate' after x epochs, 1/3 after 2x epochs, etc.
+decay_factor = 10
+
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+    initial_rate,
+    decay_steps=STEPS_PER_EPOCH*decay_factor,
+    decay_rate=1,
+    staircase=False)
+
+def get_optimizer():
+    return tf.keras.optimizers.Adam(lr_schedule, clipnorm=1.0)
+
+# model architecture
+def make_model(train_features, metrics=METRICS):
+    model = keras.Sequential([
+        # Note input shape (200, 2) when we pair features with frequency features
+        keras.layers.Dense(64,
+                           kernel_regularizer=keras.regularizers.l2(0.0001),
+                           activation='elu',
+                           input_shape=(train_features.shape[1], train_features.shape[2])),
+        keras.layers.Dense(32, kernel_regularizer=keras.regularizers.l2(0.0001),
+                     activation='elu'),
+        # keras.layers.Dense(128, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.5),
+        # keras.layers.Dense(128, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.5),
+        # keras.layers.Dense(64, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.4),
+        # keras.layers.Dense(32, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.3),
+        # keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.2),
+        # keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.2),
+        # keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.0001),
+        #              activation='elu'),
+        # keras.layers.Dropout(0.2),
+        keras.layers.Flatten(),
+        keras.layers.Dense(1, activation='sigmoid')
+        ])
+
+    model.compile(
+        optimizer=get_optimizer(),
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=metrics)
+    return model
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_auc',
+    verbose=1,
+    patience=10,
+    mode='max',
+    restore_best_weights=True)
+
+# initialize the model
+model = make_model(train_copy)
+
+# train
+# Features and labels input as numpy arrays
+model_history = model.fit(
+    train,
+    train_labels,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    # callbacks=[early_stopping],
+    validation_data=(validation, validation_labels))
+
+# Display metrics plot
+plot_metrics(model_history)
---
+++ nn_functions.py
@@ -0,0 +1,179 @@
+class FrequencyFeatures:
+    """
+    Tools to create and apply a lookup table that maps dataframe entries to the frequency of those entries in the training data. Frequencies are normalized so that maximum frequency = 1.
+
+    Attributes:
+        lookup (pandas dataframe): Dataframe were indices are entries, values are corresponding frequencies.
+        decimals (int): Number of decimal places of desired precision when rounding and binning for 'fit' and 'transform'
+    """
+
+    def __init__(self):
+        self.lookup = None
+        self.decimals = None
+
+    def bin_as_int(self, dataframe, decimals):
+        """
+        Rounds dataframe entries to desired level of precision (as a form of binning), multiplies by appropriate power of 10 and converts to ints to avoid problems with floating point errors later.
+
+        Args:
+            dataframe (pandas dataframe): Dataframe to be binned as int.
+            decimals (int): Number of decimal places of desired precision when rounding.
+
+        Returns:
+            pandas dataframe of ints
+        """
+        # multiply 'dataframe' by 10 to the power 'decimals'
+        binned_dataframe = dataframe.copy()
+        binned_dataframe = binned_dataframe*(10**decimals)
+
+        # round away all decimal places and convert to int
+        binned_dataframe = binned_dataframe.round(decimals=0).astype(int)
+
+        return binned_dataframe
+
+    def fit(self, train_dataframe, decimals):
+        """
+        Creates frequency lookup dataframe from training data and stores it as a class variable, so that it can be used to transform any dataframe. Frequencies are normalized so that max frequency = 1.
+
+        Args:
+            train_dataframe (pandas dataframe): Dataframe to use to create lookup table.
+            decimals (int): Number of decimal places of desired precision when rounding.
+
+        Returns:
+            None
+        """
+        # store decimals as class variable to be used later in 'transform'
+        self.decimals = decimals
+
+        # bin train_dataframe into bins of int type
+        binned_dataframe = self.bin_as_int(dataframe=train_dataframe, decimals=self.decimals)
+
+        # flatten 'binned_dataframe' into a single series, then count values and sort by values
+        lookup = binned_dataframe.melt()['value'].value_counts().sort_index()
+
+        # normalize by maximum
+        lookup = lookup/lookup.max()
+
+        # cut off very small values by rounding, then taking nonzero entries. Reduces size of lookup dataframe
+        # choose convention of 4 decimal places, because this is level of precision of raw data in this problem
+        lookup = lookup.round(decimals=4)
+        lookup = lookup[lookup != 0]
+
+        # linearly interpolate any missing rows, so that we can handle any unseen data
+        # create new index with no missing values from first index to last
+        filled_index = pd.RangeIndex(start=lookup.index[0], stop=lookup.index[-1] + 1)
+
+        # fill in all missing indices with NaN
+        lookup = lookup.reindex(index=filled_index)
+
+        # replace NaNs with linear interpolated values
+        lookup = lookup.interpolate()
+
+        # store as a class variable
+        self.lookup = lookup
+
+        return None
+
+    def transform(self, dataframe):
+        """
+        Creates a dataframe from 'dataframe' where entries have been mapped to frequencies according to 'self.lookup'.
+
+        Args:
+            dataframe (pandas dataframe): Dataframe to be transformed.
+
+        Returns:
+            Dataframe transformed according to 'self.lookup'.
+        """
+        dataframe = self.bin_as_int(dataframe=dataframe, decimals=self.decimals)
+
+        # transform lol according to lookup, then replace any missing values with zero (these will be on the tails of the lookup distribution)
+        # applying transformation column-wise using .apply() and .map() is a performance improvement vs. other methods such as using .replace()
+        dataframe = dataframe.apply(lambda x: x.map(self.lookup)).fillna(value=0)
+
+        return dataframe
+
+def change_class_balance(train_dataframe, train_labels, oversample_rate, majority_class_ratio):
+    """
+    Rebalance classes in the training data by oversampling classes with label 1 (duplicating, with replacement) and undersampling classes with label 0 (without replacement).
+
+    Args:
+        train_dataframe (pandas dataframe): Training data.
+        train_labels (pandas dataframe): Training data labels.
+        oversample_rate (float): Percentage of class 1 examples to duplicate.
+        majority_class_ratio (float): Multiplies class 1 size to give class 0 size.
+
+    Returns:
+        Dataframe with balanced classes, Dataframe with corresponding labels
+    """
+    train_augmented = train_dataframe.copy()
+    train_augmented['target'] = train_labels.copy()
+
+    pos_examples = train_augmented.loc[train_augmented['target']==1]
+    neg_examples = train_augmented.loc[train_augmented['target']==0]
+
+    # oversample positive examples with replacement, according to 'oversample_rate'
+    num_pos = len(pos_examples.index)
+    num_additional_pos_examples = int(oversample_rate*num_pos)
+
+    # duplicate examples to be added
+    duplicate_pos_examples = pos_examples.sample(n=num_additional_pos_examples, replace=True)
+
+    # combine duplicate positive examples with original positive examples
+    pos_examples = pd.concat([pos_examples, duplicate_pos_examples])
+
+    # choose class balance by undersampling the same number of negative examples as augmented positive examples, without replacement
+    num_neg = int(majority_class_ratio*len(pos_examples.index))
+    neg_examples = neg_examples.sample(n=num_neg, replace=False)
+
+    # combine positive and negative examples, shuffle, reindex, split off targets
+    train_augmented = pd.concat([pos_examples, neg_examples])
+    train_augmented = train_augmented.sample(frac=1, replace=False).reset_index(drop=True)
+
+    train_labels_augmented = train_augmented.pop('target')
+
+    return train_augmented, train_labels_augmented
+
+def pair_features(array):
+    """
+    Pairs each original feature value with the frequency feature value made from it.
+
+    Args:
+        array (ndarray): Numpy array of shape (number of data points, 400)
+
+    Returns:
+        Reshaped numpy array of shap (number of data points, 200, 2), where the first column of the old array is paired with the 200th column of the old array, 2nd column paired with 201st column, etc.
+    """
+    print("Initial shape ", array.shape)
+
+    # first 200 features form row 1, last 200 features form row 2, for each data point
+    array = np.reshape(array, (-1, 2, 200))
+
+    # transpose only axis 2 and 3 so that each data point is transposed
+    array = np.transpose(array, (0,2,1))
+
+    print("Final shape ", array.shape)
+
+    return array
+
+def plot_metrics(history):
+    metrics =  ['loss', 'auc', 'precision', 'recall']
+    plt.rcParams['figure.figsize'] = (12, 10)
+    plt.figure(figsize=(6,4))
+
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_"," ").capitalize()
+        plt.subplot(2,2,n+1)
+        plt.plot(history.epoch,  history.history[metric], color='b', label='Train')
+        plt.plot(history.epoch, history.history['val_'+metric], color='g', linestyle="--", label='Val')
+        plt.xlabel('Epoch')
+        plt.ylabel(name)
+        if metric == 'loss':
+            plt.ylim([0, plt.ylim()[1]])
+        elif metric == 'auc':
+            plt.ylim([0.8,1])
+        else:
+            plt.ylim([0,1])
+
+    plt.legend()
+    plt.savefig('metrics.png')
+    plt.close()
